6d1a3a9c09310eb89264eac9b8ad20cf12845539
lauraalyson <lauraalyson3@gmail.com>
2021-12-06 16:03:53 -0500

    add draggable divs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { motion } from "framer-motion";
+import { useDragControls } from "framer-motion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const constraintsRef = React.useRef(null);
+    const dragControls = useDragControls();
+
+    return (
+			<div
+				className='area'
+				ref={constraintsRef}
+				onPointerDown={(event) => dragControls.start(event)}>
+				<motion.div
+					className='box'
+					drag
+					dragConstraints={constraintsRef}
+					dragControls={dragControls}
+				/>
+				<motion.div
+					className='box'
+					drag
+					dragConstraints={constraintsRef}
+					dragControls={dragControls}
+				/>
+			</div>
+		)
 }
 
-export default App;
+export default App
---
+++ src/components/draggable-grid.js
@@ -0,0 +1,11 @@
+import React from "react"
+import './../App.css'
+
+function DraggableGrid() {
+	return (
+		<section>
+		</section>
+	)
+}
+
+export default DraggableGrid
