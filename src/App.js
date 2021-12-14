80b15442e54a3dde9f072e73c76a28f3e9207a29
lauraalyson <lauraalyson3@gmail.com>
2021-12-14 13:25:06 -0500

    clean app file for react-grid-layout implementation

--- src/App.js
+++ src/App.js
@@ -1,73 +1,12 @@
 import React from "react";
 import "./App.css";
-import { motion } from "framer-motion";
-import { useDragControls } from "framer-motion";
-import Map from "./components/map";
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
 
 function App() {
-    const constraintsRef = React.useRef(null);
-    const dragControls = useDragControls();
 
     return (
-			<div
-				className='area'
-				ref={constraintsRef}
-				onPointerDown={(event) => dragControls.start(event)}>
-				<div>
-					<motion.div
-						className='box-square'
-						drag
-						dragConstraints={constraintsRef}
-						dragControls={dragControls}
-					><Map /></motion.div>
-					<motion.div
-						className='box-square v-1'
-						drag
-						dragConstraints={constraintsRef}
-						dragControls={dragControls}
-					/>
-				</div>
-				<motion.div
-					className='box-tall'
-					drag
-					dragConstraints={constraintsRef}
-					dragControls={dragControls}
-				/>
-				<div>
-					<motion.div
-						className='box-wide'
-						drag
-						dragConstraints={constraintsRef}
-						dragControls={dragControls}
-					/>
-					<div>
-						<motion.div
-							className='box-wide v-2'
-							drag
-							dragConstraints={constraintsRef}
-							dragControls={dragControls}
-						/>
-					</div>
-				</div>
-
-				<motion.div
-					className='box-square'
-					drag
-					dragConstraints={constraintsRef}
-					dragControls={dragControls}
-				/>
-				<motion.div
-					className='box-wide v-3'
-					drag
-					dragConstraints={constraintsRef}
-					dragControls={dragControls}
-				/>
-				<motion.div
-					className='box-square'
-					drag
-					dragConstraints={constraintsRef}
-					dragControls={dragControls}
-				/>
+			<div>
 			</div>
 		)
 }
