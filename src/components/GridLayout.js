7b399f348488a482990c4cf5f1c2731cd4d6f06f
lauraalyson <lauraalyson3@gmail.com>
2021-12-15 21:14:42 -0500

    change breakpoints to be more specific

--- src/components/GridLayout.js
+++ src/components/GridLayout.js
@@ -8,125 +8,6 @@ import { Responsive, WidthProvider } from "react-grid-layout";
 import "./../App.css"
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
-
-export default class ShowcaseLayout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentBreakpoint: "lg",
-      compactType: "vertical",
-      mounted: false,
-      layouts: { lg: props.initialLayout }
-    };
-
-    this.onBreakpointChange = this.onBreakpointChange.bind(this);
-    this.onCompactTypeChange = this.onCompactTypeChange.bind(this);
-    this.onLayoutChange = this.onLayoutChange.bind(this);
-    this.onNewLayout = this.onNewLayout.bind(this);
-  }
-
-  componentDidMount() {
-    this.setState({ mounted: true });
-  }
-
-  generateDOM() {
-    return _.map(this.state.layouts.lg, function(l, i) {
-      return (
-        <div key={i}>
-            <div className="text grid-container">
-                <div>
-                 <DivFill />
-                </div>
-            </div>
-        </div>
-      );
-    });
-  }
-
-  onBreakpointChange(breakpoint) {
-    this.setState({
-      currentBreakpoint: breakpoint
-    });
-  }
-
-  onCompactTypeChange() {
-    const { compactType: oldCompactType } = this.state;
-    const compactType =
-      oldCompactType === "horizontal"
-        ? "vertical"
-        : oldCompactType === "vertical"
-          ? null
-          : "horizontal";
-    this.setState({ compactType });
-  }
-
-  onLayoutChange(layout, layouts) {
-    this.props.onLayoutChange(layout, layouts);
-  }
-
-  onNewLayout() {
-    this.setState({
-      layouts: { lg: generateLayout() }
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <ResponsiveReactGridLayout
-          {...this.props}
-          layouts={this.state.layouts}
-          onBreakpointChange={this.onBreakpointChange}
-          onLayoutChange={this.onLayoutChange}
-          measureBeforeMount={false}
-          useCSSTransforms={this.state.mounted}
-          compactType={this.state.compactType}
-          preventCollision={!this.state.compactType}
-        >
-          {this.generateDOM()}
-        </ResponsiveReactGridLayout>
-      </div>
-    );
-  }
-}
-
-ShowcaseLayout.propTypes = {
-  onLayoutChange: PropTypes.func.isRequired
-};
-
-ShowcaseLayout.defaultProps = {
-  className: "layout",
-  rowHeight: 30,
-  onLayoutChange: function() {},
-  cols: { lg: 6, md: 6, sm: 6, xs: 4, xxs: 4 },
-  initialLayout: generateLayout()
-};
-
-
-function generateLayout() {
-    const layout = [
-        { i: '0', x: 0, y: 0, w: 4, h: 7 },
-        { i: '1', x: 4, y: 0, w: 2, h: 7 },
-        { i: '2', x: 0, y: 9, w: 2, h: 7 },
-        { i: '3', x: 2, y: 9, w: 2, h: 7 },
-        { i: '4', x: 4, y: 9, w: 2, h: 14 },
-        { i: '5', x: 0, y: 18, w: 4, h: 7 },
-        { i: '6', x: 0, y: 27, w: 2, h: 7 },
-        { i: '7', x: 2, y: 27, w: 4, h: 7 },
-    ]
-
-    return layout.map((layout) => {
-        return {
-            i: layout.i,
-            x: layout.x,
-            y: layout.y,
-            w: layout.w,
-            h: layout.h
-        }
-    })
-}
-
-
 const layoutConfig = {
 	lg: [
 		{ i: '0', x: 0, y: 0, w: 4, h: 7 },
@@ -156,7 +37,7 @@ const layoutConfig = {
 		{ i: '4', x: 0, y: 18, w: 2, h: 14 },
 		{ i: '5', x: 0, y: 9, w: 4, h: 7 },
 		{ i: '6', x: 2, y: 18, w: 2, h: 7 },
-		{ i: '7', x: 2, y: 27, w: 4, h: 7 },
+		{ i: '7', x:0, y: 27, w: 4, h: 7 },
 	],
 	xs: [
 		{ i: '0', x: 0, y: 0, w: 4, h: 7 },
@@ -166,7 +47,7 @@ const layoutConfig = {
 		{ i: '4', x: 0, y: 18, w: 2, h: 10 },
 		{ i: '5', x: 0, y: 9, w: 4, h: 7 },
 		{ i: '6', x: 2, y: 18, w: 2, h: 5 },
-		{ i: '7', x: 2, y: 27, w: 4, h: 7 },
+		{ i: '7', x: 0, y: 27, w: 4, h: 7 },
 	],
 	xxs: [
 		{ i: '0', x: 0, y: 0, w: 4, h: 7 },
@@ -176,6 +57,172 @@ const layoutConfig = {
 		{ i: '4', x: 0, y: 18, w: 2, h: 10 },
 		{ i: '5', x: 0, y: 9, w: 4, h: 7 },
 		{ i: '6', x: 2, y: 18, w: 2, h: 5 },
-		{ i: '7', x: 2, y: 27, w: 4, h: 7 },
+		{ i: '7', x: 0, y: 27, w: 4, h: 7 },
 	],
+}
+
+export default class ShowcaseLayout extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			currentBreakpoint: '',
+			compactType: 'vertical',
+			mounted: false,
+			layouts: layoutConfig,
+		}
+
+		this.onBreakpointChange = this.onBreakpointChange.bind(this)
+		// this.onCompactTypeChange = this.onCompactTypeChange.bind(this)
+		this.onLayoutChange = this.onLayoutChange.bind(this)
+		this.onNewLayout = this.onNewLayout.bind(this);
+	}
+
+	componentDidMount() {
+		this.setState({ mounted: true })
+	}
+
+	generateDOM() {
+		return _.map(this.state.layouts.lg, function (l, i) {
+			return (
+				<div key={i}>
+					<div className='text grid-container'>
+						<div key='1'>
+							<DivFill />
+						</div>
+					</div>
+				</div>
+			)
+		})
+	}
+
+	onBreakpointChange(breakpoint) {
+		this.setState({
+			currentBreakpoint: breakpoint,
+		})
+	}
+
+	// onCompactTypeChange() {
+	// 	const { compactType: oldCompactType } = this.state
+	// 	const compactType =
+	// 		oldCompactType === 'horizontal'
+	// 			? 'vertical'
+	// 			: oldCompactType === 'vertical'
+	// 			? null
+	// 			: 'horizontal'
+	// 	this.setState({ compactType })
+	// }
+
+	onLayoutChange = (layout, layouts) => {
+		this.setState({ layouts })
+	}
+
+	  onNewLayout() {
+	    this.setState({
+	      layouts: { lg: generateLayout() }
+	    });
+	  }
+
+	render() {
+		return (
+			<div>
+				<ResponsiveReactGridLayout
+					{...this.props}
+					layouts={{
+						lg: layoutConfig.lg,
+						md: layoutConfig.md,
+						sm: layoutConfig.sm,
+                        xs: layoutConfig.xs,
+                        xxs: layoutConfig.xxs
+					}}
+					onBreakpointChange={this.onBreakpointChange}
+					onLayoutChange={this.onLayoutChange}
+					measureBeforeMount={false}
+					useCSSTransforms={this.state.mounted}
+					compactType={this.state.compactType}
+					preventCollision={!this.state.compactType}>
+
+					{this.generateDOM()}
+
+				</ResponsiveReactGridLayout>
+			</div>
+		)
+	}
+}
+
+ShowcaseLayout.propTypes = {
+  onLayoutChange: PropTypes.func.isRequired
+};
+
+ShowcaseLayout.defaultProps = {
+  className: "layout",
+  rowHeight: 30,
+  onLayoutChange: function() {},
+  cols: { lg: 6, md: 6, sm: 6, xs: 4, xxs: 4 },
+  breakpoints: { lg: 1000, md: 750, sm: 700, xs: 475, xxs: 450 }
+//   initialLayout: generateLayout()
+};
+
+function generateLayout() {
+    return {
+			lg: [
+				{ i: '0', x: 0, y: 0, w: 4, h: 7 },
+				{ i: '1', x: 4, y: 0, w: 2, h: 7 },
+				{ i: '2', x: 0, y: 9, w: 2, h: 7 },
+				{ i: '3', x: 2, y: 9, w: 2, h: 7 },
+				{ i: '4', x: 4, y: 9, w: 2, h: 14 },
+				{ i: '5', x: 0, y: 18, w: 4, h: 7 },
+				{ i: '6', x: 0, y: 27, w: 2, h: 7 },
+				{ i: '7', x: 2, y: 27, w: 4, h: 7 },
+			],
+			md: [
+				{ i: '0', x: 0, y: 0, w: 4, h: 7 },
+				{ i: '1', x: 4, y: 0, w: 2, h: 7 },
+				{ i: '2', x: 0, y: 9, w: 2, h: 7 },
+				{ i: '3', x: 2, y: 9, w: 2, h: 7 },
+				{ i: '4', x: 4, y: 9, w: 2, h: 14 },
+				{ i: '5', x: 0, y: 18, w: 4, h: 7 },
+				{ i: '6', x: 0, y: 27, w: 2, h: 7 },
+				{ i: '7', x: 2, y: 27, w: 4, h: 7 },
+			],
+			sm: [
+				{ i: '0', x: 0, y: 0, w: 4, h: 7 },
+				{ i: '1', x: 4, y: 0, w: 2, h: 7 },
+				{ i: '2', x: 0, y: 9, w: 2, h: 7 },
+				{ i: '3', x: 2, y: 9, w: 2, h: 7 },
+				{ i: '4', x: 4, y: 9, w: 2, h: 14 },
+				{ i: '5', x: 0, y: 18, w: 4, h: 7 },
+				{ i: '6', x: 0, y: 27, w: 2, h: 7 },
+				{ i: '7', x: 2, y: 27, w: 4, h: 7 },
+			],
+			sm: [
+				{ i: '0', x: 0, y: 0, w: 4, h: 7 },
+				{ i: '1', x: 0, y: 0, w: 2, h: 7 },
+				{ i: '2', x: 2, y: 0, w: 2, h: 7 },
+				{ i: '3', x: 2, y: 9, w: 2, h: 7 },
+				{ i: '4', x: 0, y: 18, w: 2, h: 14 },
+				{ i: '5', x: 0, y: 9, w: 4, h: 7 },
+				{ i: '6', x: 2, y: 18, w: 2, h: 7 },
+				{ i: '7', x: 0, y: 27, w: 4, h: 7 },
+			],
+			xs: [
+				{ i: '0', x: 0, y: 0, w: 4, h: 7 },
+				{ i: '1', x: 0, y: 0, w: 2, h: 5 },
+				{ i: '2', x: 2, y: 0, w: 2, h: 5 },
+				{ i: '3', x: 2, y: 9, w: 2, h: 5 },
+				{ i: '4', x: 0, y: 18, w: 2, h: 10 },
+				{ i: '5', x: 0, y: 9, w: 4, h: 7 },
+				{ i: '6', x: 2, y: 18, w: 2, h: 5 },
+				{ i: '7', x: 0, y: 27, w: 4, h: 7 },
+			],
+			xxs: [
+				{ i: '0', x: 0, y: 0, w: 4, h: 7 },
+				{ i: '1', x: 0, y: 0, w: 2, h: 5 },
+				{ i: '2', x: 2, y: 0, w: 2, h: 5 },
+				{ i: '3', x: 2, y: 9, w: 2, h: 5 },
+				{ i: '4', x: 0, y: 18, w: 2, h: 10 },
+				{ i: '5', x: 0, y: 9, w: 4, h: 7 },
+				{ i: '6', x: 2, y: 18, w: 2, h: 5 },
+				{ i: '7', x: 0, y: 27, w: 4, h: 7 },
+			],
+		}
 }
