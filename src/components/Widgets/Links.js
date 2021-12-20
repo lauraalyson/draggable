ef534f092a53a50e70b8883f4935b6024d2983fc
lauraalyson <lauraalyson3@gmail.com>
2021-12-20 12:42:17 -0500

    add accessibility tags to imgs

--- src/components/Widgets/Links.js
+++ src/components/Widgets/Links.js
@@ -20,7 +20,7 @@ class Links extends React.Component {
 						href='https://github.com/lauraalyson'
 						target='_blank'
 						rel='noreferrer noopener'>
-						<img src={github} />
+						<img src={github} alt='github-logo' />
 					</a>
 				</div>
 
@@ -29,13 +29,13 @@ class Links extends React.Component {
 						href='https://www.linkedin.com/in/laura-waterbury/'
 						target='_blank'
 						rel='noreferrer noopener'>
-						<img src={linkedin} />
+						<img src={linkedin} alt='linkedin-logo' />
 					</a>
 				</div>
 
 				<div className='col-5'>
 					<a href='mailto:lauraalyson3@gmail.com'>
-						<img src={email} />
+						<img src={email} alt='email-logo' />
 					</a>
 				</div>
 
@@ -44,7 +44,7 @@ class Links extends React.Component {
 						href='https://codepen.io/lauraalyson'
 						target='_blank'
 						rel='noreferrer noopener'>
-						<img src={codepen} />
+						<img src={codepen} alt='codepen-logo' />
 					</a>
 				</div>
 			</div>
